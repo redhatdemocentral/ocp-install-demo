2271a55923d419636acafd176e072f4842c9f083
Eric D. Schabell <eric@schabell.org>
2016-11-17 16:30:40 +0100

    Added docker and oc checks, added version checks and more reporting on finish. Fixes #1. Fixes #2.

--- init.sh
+++ init.sh
@@ -39,16 +39,45 @@ if [[ `uname` == 'Darwin' ]]; then
 		echo
 fi
 
-if [[ `uname` == 'Darwin' ]]; then
-	command -v oc help >/dev/null 2>&1 || { echo >&2 "OpenShift CLI tooling is required but not installed yet... downlaod here: https://s3.amazonaws.com/oso-preview-docker-registry/client-tools/3.3/oc-3.3.1.3-1-macosx.tar.gz"; exit 1; }
-	echo "OpenShift command line tools installed..."
+# Ensure docker engine available.
+#
+command -v docker -h >/dev/null 2>&1 || { echo >&2 "Docker is required but not installed yet... downlaod here: https://www.docker.com/products/docker"; exit 1; }
+echo "Docker is installed... checking for valid version..."
+echo
+		
+# Check docker enging version.
+dockerverone=$(docker version | awk '/Version:/{print $2}' | awk -F[=.] '{print $1}')
+dockervertwo=$(docker version | awk '/Version:/{print $2}' | awk -F[=.] '{print $2}')
+if [[ $dockerverone -eq 1 ]] && [[ $dockervertwo -ge 10 ]]; then
+	echo
+	echo "Valid version of docker engine found..."
 	echo
 else
-		# Linux machine.
-		#
-		command -v oc help >/dev/null 2>&1 || { echo >&2 "OpenShift CLI tooling is required but not installed yet... downlaod here: https://s3.amazonaws.com/oso-preview-docker-registry/client-tools/3.3/oc-3.3.1.3-1-linux.tar.gz"; exit 1; } 
-		echo "OpenShift command line tools installed..."
-		echo
+	echo "Docker engine version $dockerverone.$dockervertwo found... need 1.10+, please update: https://www.docker.com/products/docker"
+	echo
+	exit
+fi
+
+# Ensure OpenShift command line tools available.
+#
+command -v oc help >/dev/null 2>&1 || { echo >&2 "OpenShift CLI tooling is required but not installed yet... downlaod here: https://s3.amazonaws.com/oso-preview-docker-registry/client-tools/3.3/oc-3.3.1.3-1-macosx.tar.gz"; exit 1; }
+echo "OpenShift command line tools installed... checking for valid version..."
+echo
+
+# Check oc version.
+verone=$(oc version | awk '/oc/{print $2}' | awk -F[=.] '{print $1}')
+vertwo=$(oc version | awk '/oc/{print $2}' | awk -F[=.] '{print $2}')
+if [[ $verone == 'v3' ]] && [[ $vertwo -eq 3 ]]; then
+	echo "Version of installed OpenShift command line tools correct..."
+	echo
+else
+	echo "Version of installed OpenShift command line tools is $verone.$vertwo, must be v3.3 or higher..."
+	echo
+	echo "Downlaod for Linux here: https://s3.amazonaws.com/oso-preview-docker-registry/client-tools/3.3/oc-3.3.0.35-1-linux.tar.gz"
+	echo "Downlaod for Mac here: https://s3.amazonaws.com/oso-preview-docker-registry/client-tools/3.3/oc-3.3.1.3-1-macosx.tar.gz"
+	echo
+	echo
+	exit
 fi
 
 
@@ -56,12 +85,11 @@ echo "Installing OSE with cluster up..."
 echo
 oc cluster up --image=registry.access.redhat.com/openshift3/ose --version=v3.3.1.3 --create-machine
 
-
 if [ $? -ne 0 ]; then
 		echo
 		echo Error occurred during 'oc cluster up' command!
 		echo
-		echo Cleaning out existing 'openshift' docker machine.
+		echo Cleaning out existing 'openshift' machine.
 		echo
 		docker-machine rm -f openshift
 
@@ -101,30 +129,28 @@ if [ $? -ne 0 ]; then
 		fi
 fi
 
+
 echo
 echo "Logging in as admin user..."
 echo
 oc login -u system:admin
 
-LINE=( `oc status | grep https` )
-OCP_IP=${LINE[7]}
+# capturing OCP IP address from status command.
+OCP_IP=$(oc status | awk '/My Project/{print $8}')
 
 echo 
 echo "Set OCP IP to $OCP_IP..."
 echo
 
-echo
 echo "Updating JBoss image streams..."
 echo
 oc delete -n openshift -f 'https://raw.githubusercontent.com/jboss-openshift/application-templates/master/jboss-image-streams.json'
-echo
 oc create -n openshift -f 'https://raw.githubusercontent.com/jboss-openshift/application-templates/master/jboss-image-streams.json'
 
 echo
 echo "Updating RHEL 7 image streams..."
 echo
 oc delete -n openshift -f 'https://raw.githubusercontent.com/openshift/openshift-ansible/master/roles/openshift_examples/files/examples/v1.3/image-streams/image-streams-rhel7.json'
-echo
 oc create -n openshift -f 'https://raw.githubusercontent.com/openshift/openshift-ansible/master/roles/openshift_examples/files/examples/v1.3/image-streams/image-streams-rhel7.json'
 
 echo
@@ -135,7 +161,7 @@ echo "= Look for information at end of OSE install.      ="
 echo "=                                                  ="
 echo "=  The server is accessible via web console at:    ="
 echo "=                                                  ="
-echo "=	    $OCP_IP            ="
+echo "=	    $OCP_IP                  ="
 echo "=                                                  ="
 echo "=	 Log in as user: openshift-dev             ="
 echo "=    password: devel                               ="
@@ -151,5 +177,12 @@ echo "=                                                  ="
 echo "=  When finished, clean up your demo with:         ="
 echo "=                                                  ="
 echo "=     $ docker-machine rm -f openshift             ="
+echo "=                                                  ="
+echo "=  If you want to save the machine and restart it  ="
+echo "=  next time, don't use init.sh, instead:          ="
+echo "=                                                  ="
+echo "=     $ docker-manchine stop openshift             ="
+echo "=                                                  ="
+echo "=     $ docker-machine start openshift             ="
 echo "=                                                  ="
 echo "===================================================="
